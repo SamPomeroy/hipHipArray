57ed64c71b3514519861377216547aa275e470dc
Samantha Pomeroy <samanthapomeroy38@gmail.com>
2023-12-12 12:36:42 -0600

    almost completed

--- main.js
+++ main.js
@@ -22,12 +22,18 @@ const students = [
   'Arnold',
   'Andy'
 ];
+for(let i = 0; i < students.length; i ++){
+  // console.log(students[i])
+}
 
 
 // Challenge 2
 // Loop through the following array BACKWARDS, console.logging out each value. 64 should be printed first and 100 last.
 const grades = [100, 80, 110, 75, 83, 64];
+for(let i = grades.length -1; i >= 0; i --){
+  // console.log(grades[i])
 
+}
 
 // Challenge 3
 // Console.log out only the even numbers in the following array.
@@ -37,36 +43,87 @@ const positiveNumbers = [5, 2, 13, 17, 4, 102, 3000];
 // Challenge 4
 // Console.log out the even numbers in the following array, INCLUDING the negative ones. There should be four total printings.
 const mixedSignNumbers = [3, 15, 14, -2, -3, -8, -103, 4];
-
+let evenNums = []
+// for(let i= 0; i < mixedSignNumbers.length; i++){
+//   if(mixedSignNumbers[i] % 2 === 0){
+//     evenNums.push(mixedSignNumbers[i]);
+//   }console.log(evenNums);
+// }
 
 // Challenge 5
 // Remove two values from the beginning and one value from the end of the following array, simply by deleting them in the following line. Then console.log out each value individually.
 const symmetricalCapitals = ['A', 'H', 'I', 'M', 'O', 'T', 'U', 'V', 'W', 'X', 'Y'];
 
+symmetricalCapitals.shift()
+symmetricalCapitals.shift()
+symmetricalCapitals.pop()
+// console.log(symmetricalCapitals)
 
 // Challenge 6
 // Add a value (your choice!) to the start of the following array and two values (of your choice) to the end simply by adding them in the following line, and then console.log out each value individually.
 const fibonacciNumbers = [1, 1, 2, 3, 5, 8, 13];
 
+fibonacciNumbers.unshift(7)
+fibonacciNumbers.push(9, 14)
+// console.log(fibonacciNumbers[0])
+// console.log(fibonacciNumbers[8])
+// console.log(fibonacciNumbers[9])
 
 // Challenge 7
 // Make a NEW array with 5 values of your choice, using the same syntax as the array creation from the previous 6 challenges; that is, every value should be placed within the square brackets.  Now console.log out each value individually.
+let books = ['vol1', 'vol2', 'vol3', 'vol4', 'vol5']
 
+// console.log(books[0])
+// console.log(books[1])
+// console.log(books[2])
+// console.log(books[3])
+// console.log(books[4])
 
 // Challenge 8
 // Make a new EMPTY array and fill it with five values of your choice using BOTH .push AND .unshift, and then console.log out each value individually.
-
+let emptyBucket = []
+emptyBucket.push('pos1', 'pos2', 'pos3', 'pos4')
+emptyBucket.unshift('pos0')
+// console.log(emptyBucket[0])
+// console.log(emptyBucket[1])
+// console.log(emptyBucket[2])
+// console.log(emptyBucket[3])
+// console.log(emptyBucket[4])
 
 // Challenge 9
 // Loop through the `students` array from Challenge 1, starting at index 3 and ending at index 10 (INCLUSIVE), logging out each value individually.
 
+for(let i = 3; i <= 10; i ++){
+// console.log(students[i])
+}
 
 // Challenge 10
 // Loop through the `students` array from Challenge 1, making a COPY of the array, starting at index 3 and ending at index 10 (INCLUSIVE). This is very similar to string building; create a new array, loop through the old one, and put values from the original in the new one. Then log out each value of the copy indivudally.
 
+// let newList = []
+// for(let i = 3; i <= 10; i ++){
+//   newList.push(students[i])
+//   }
+//   console.log(newList)
+// for(let i = 0; i < newList.length; i++){
+//     console.log(newList[i])
+//   }
+
+
 
 // Challenge 11
 // Make a COPY of the `students` array *using .slice*, starting at index 3 and ending at index 10 (INCLUSIVE), then log out each value of the copy individually. THEN log out each value of the original array. Notice that the original array didn't get any values taken out of it, because `slice` does NOT modify the original array.
+let newList = students.slice(3, 11)
+
+console.log(newList[0])
+console.log(newList[1])
+console.log(newList[2])
+console.log(newList[3])
+console.log(newList[4])
+console.log(newList[5])
+console.log(newList[6])
+console.log(newList[7])
+console.log(students)
 
 
 // Challenge 12
